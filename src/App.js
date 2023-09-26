b8fd74eea22332ada918eb8aaaab99a05c485896
Sahil Saini <76941329+sahilsaini1107@users.noreply.github.com>
2023-09-26 05:50:43 +0000

    added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useEffect } from 'react';
 import Title from './components/Title';
+import Setup from './examples/1-first-request'
+
 function App() {
   return (
     <main>
       <Title />
+      <Setup/>
     </main>
   );
 }
--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,10 +1,22 @@
 import { useEffect } from 'react';
 // limit, if 429 wait for 15 min and try again
+import axios from 'axios' 
 const url = 'https://course-api.com/react-store-products';
 
 const FirstRequest = () => {
+  const fetchdata = async ()=>{
+    try {
+      const res = await axios.get(url)
+      const data = res.data
+      console.log(data)
+    } catch (error) {
+      console.log(error.response)
+    }
+  };
+
   useEffect(() => {
     console.log('first axios request');
+    fetchdata()
   }, []);
 
   return <h2 className='text-center'>first request</h2>;
